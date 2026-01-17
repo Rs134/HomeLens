2d5bf89c466a5540f0c9235c0c7f6932cb00a082
Reiad <reiadwc@gmail.com>
2026-01-16 20:38:25 -0500

    Drafted the about section

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,67 @@
+function About(){
+    return (
+        <div className="about">
+            <div className="about-container">
+                <div className="about-headline">
+                    <div className="about-left-heading">
+                        <h2> Discover What We Can Build Together</h2>
+                    </div>
+                    <div className="about-right-heading">
+                        <h3> Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem voluptatibus voluptatum obcaecati nesciunt minima reprehenderit aperiam repudiandae vel placeat a non omnis nulla ullam in, error voluptate corrupti optio ipsum!</h3>
+                    </div>
+                </div>
+                <div className="cards-container">
+                    <div className="card">
+                        <div className="card-icon">
+
+                        </div>
+                        <div className="card-heading">
+                            <h3> AI Driven Filtering </h3>
+
+                        </div>
+                        <div className="card-content">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quas amet voluptatum cupiditate sed aliquam magni laboriosam necessitatibus esse? Tempore tempora deserunt expedita consequuntur nam asperiores. Officiis eligendi possimus deleniti.</p>
+
+                        </div>
+
+                    </div>
+
+                    <div className="card">
+                        <div className="card-icon">
+
+                        </div>
+                        <div className="card-heading">
+                            <h3> AI Driven Filtering </h3>
+
+                        </div>
+                        <div className="card-content">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quas amet voluptatum cupiditate sed aliquam magni laboriosam necessitatibus esse? Tempore tempora deserunt expedita consequuntur nam asperiores. Officiis eligendi possimus deleniti.</p>
+
+                        </div>
+
+                    </div>
+
+                    <div className="card">
+                        <div className="card-icon">
+
+                        </div>
+                        <div className="card-heading">
+                            <h3> AI Driven Filtering </h3>
+
+                        </div>
+                        <div className="card-content">
+                            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quas amet voluptatum cupiditate sed aliquam magni laboriosam necessitatibus esse? Tempore tempora deserunt expedita consequuntur nam asperiores. Officiis eligendi possimus deleniti.</p>
+
+                        </div>
+
+                    </div>
+
+                    
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
